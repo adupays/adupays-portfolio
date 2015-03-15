26a75610964bf1a13ef32005d46f20afc2cbe622
Amandine Dupays <amandine.dupays@gmail.com>
2015-03-15 13:39:54 +0100

    update about.jpg and cv

--- lib/db.php
+++ lib/db.php
@@ -2,7 +2,8 @@
 // Gérer l'erreur de connection à la BDD
 try {
     // se connecter à la BDD
-    $db = new PDO('mysql:host=mysql51-111.perso;dbname=amandinezbdd', 'amandinezbdd', '8MuCsn6SaqJD', array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+    // $db = new PDO('mysql:host=mysql51-111.perso;dbname=amandinezbdd', 'amandinezbdd', '8MuCsn6SaqJD', array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+    $db = new PDO('mysql:host=localhost;dbname=amandinezbdd', 'root', 'admin');
 
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // formate le fetch comme un tableau
     // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
